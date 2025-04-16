d9c8ad422fe7feaaf1679a27c9abfff0355a5fa4
[BK] Phù Dung <husthunter97@gmail.com>
2025-04-16 17:14:21 +0700

    change primary color app

--- src/constants/styles.ts
+++ src/constants/styles.ts
@@ -1,8 +1,8 @@
 export const GlobalStyles = {
   colors: {
-    primary100: "#f9beda",
-    primary500: "#c30b64",
-    primary800: "#610440",
+    primary100: "#d1fae5",
+    primary500: "#10b981",
+    primary800: "#065f46",
     error100: "#fcdcbf",
     error500: "#f37c13",
   },
